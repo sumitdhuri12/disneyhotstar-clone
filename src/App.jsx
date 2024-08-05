ab541d819708ea7c9756c8a3930813e062dc4117
sumitdhuri12 <sumit.s.dhuri@gmail.com>
2024-08-05 21:32:15 +0530

    Final revert

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Components/Header'
+import Slider from './Components/Slider'
+import ProductionHouse from './Components/ProductionHouse'
+import GenreMovieList from './Components/GenreMovieList'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Slider/>
+      <ProductionHouse/>
+      <GenreMovieList/>
+    </>
+  )
+}
+
+export default App
