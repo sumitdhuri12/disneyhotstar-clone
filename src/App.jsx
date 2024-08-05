13c823fa4f22e8c602efb36ce282e2d03699e6ef
sumitdhuri12 <sumit.s.dhuri@gmail.com>
2024-08-05 21:28:41 +0530

    Reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
@@ -10,12 +7,12 @@ import GenreMovieList from './Components/GenreMovieList'
 function App() {
 
   return (
-    <>
+    <div className=''>
       <Header/>
       <Slider/>
       <ProductionHouse/>
       <GenreMovieList/>
-    </>
+    </div>
   )
 }
 
