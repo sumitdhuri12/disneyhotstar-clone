d314ed86d8a80b5a3228b881169fda0a4eda9146
sumitdhuri12 <sumit.s.dhuri@gmail.com>
2024-08-05 02:54:43 +0000

    moved files to root

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './src/Components/Header'
+import Slider from './src/Components/Slider'
+import ProductionHouse from './src/Components/ProductionHouse'
+import GenreMovieList from './src/Components/GenreMovieList'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Slider/>
+      <ProductionHouse/>
+      <GenreMovieList/>
+    </>
+  )
+}
+
+export default App
